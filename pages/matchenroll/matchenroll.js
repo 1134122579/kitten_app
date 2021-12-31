bb1f628c95a46ebd3370e0438e8a5185889aa1ac
wml201314 <1134122579@qq.com>
2021-12-31 10:55:10 +0800

    sai shi bao ming

--- pages/matchenroll/matchenroll.js
+++ pages/matchenroll/matchenroll.js
@@ -1,5 +1,5 @@
 // pages/enrollpage/enrollpage.js
-let App=getApp()
+let App = getApp()
 import Api from "../../api/index";
 import {
   getDate
@@ -12,24 +12,24 @@ Page({
    */
   data: {
     match_id: '',
-    cat_pz:'',
+    cat_pz: '',
     sex: null, //性别
     cat_name: "", //姓名
-    color:'',//颜色
+    color: '', //颜色
     birthday: "", //生日
-    eye_color:"",//眼睛颜色
-    register_no:"",//编号
-    father_name:'',//
-    father_pz:'',
-    father_color:'',
-    father_register_no:'',
-    mother_pz:'',
-    mother_color:'',
-    mother_name:'',
-    mother_register_no:'',
-    group_id:'',//组别*
-    match_id:'',//FUN SHOW
-    desc:'',
+    eye_color: "", //眼睛颜色
+    register_no: "", //编号
+    father_name: '', //
+    father_pz: '',
+    father_color: '',
+    father_register_no: '',
+    mother_pz: '',
+    mother_color: '',
+    mother_name: '',
+    mother_register_no: '',
+    group_id: '', //组别*
+    match_id: '', //FUN SHOW
+    desc: '',
     level: '',
     levelIndex: null,
     sexIndex: null,
@@ -87,7 +87,7 @@ Page({
       success(res) {
         // 上传完成需要更新 fileList
         res = JSON.parse(res.data);
-        console.log(res ,"upImage");
+        console.log(res, "upImage");
         let fileList = [{
           ...file,
           url: res.data.imgLink
@@ -96,7 +96,7 @@ Page({
           fileList
         });
       },
-      complete(){
+      complete() {
         wx.hideLoading()
       }
     });
@@ -107,16 +107,16 @@ Page({
       fileList: [],
     })
   },
-    // 品种
-    bindpzChange(event) {
-      let {
-        PzList
-      } = this.data
-      this.setData({
-        // sexIndex: event.detail.value,
-        cat_pz: PzList[event.detail.value]['text']
-      })
-    },
+  // 品种
+  bindpzChange(event) {
+    let {
+      PzList
+    } = this.data
+    this.setData({
+      // sexIndex: event.detail.value,
+      cat_pz: PzList[event.detail.value]['text']
+    })
+  },
   // 性别
   bindsexChange(event) {
     let {
@@ -156,24 +156,86 @@ Page({
   // 提交
   onClick() {
     let {
-      cat_name,
-      fileList,
-      sex,
       level,
-      desc,
-      match_id
+      cat_pz,
+      sex,
+      cat_name,
+      color,
+      birthday,
+      eye_color,
+      register_no,
+      father_name,
+      father_pz,
+      father_color,
+      father_register_no,
+      mother_pz,
+      mother_color,
+      mother_name,
+      mother_register_no,
+      group_id,
+      match_id,
     } = this.data
-    let img = fileList?.[0]?.url
-    if (!img) {
+    if (this.checkUpQuery()) {
+      wx.showLoading({
+        title: '报名中..',
+        mask: true
+      })
+      Api.joinMatch(this.data).then(res => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '参赛成功，1.5秒自动返回',
+          icon: "none",
+          mask: true,
+        })
+        setTimeout(() => {
+          wx.navigateBack({
+            delta: 1,
+          })
+        }, 1500);
+      })
+    }
+
+  },
+  // 校验上传数据
+  checkUpQuery() {
+    let {
+      level,
+      cat_pz,
+      sex,
+      cat_name,
+      color,
+      birthday,
+      eye_color,
+      register_no,
+      father_name,
+      father_pz,
+      father_color,
+      father_register_no,
+      mother_pz,
+      mother_color,
+      mother_name,
+      mother_register_no,
+      group_id,
+      match_id,
+    } = this.data
+    // let img = fileList?.[0]?.url
+    // if (!img) {
+    //   wx.showToast({
+    //     title: "请上传图片",
+    //     icon: "none",
+    //   });
+    //   return;
+    // }
+    if (!cat_name.trim()) {
       wx.showToast({
-        title: "请上传图片",
+        title: "请输入姓名",
         icon: "none",
       });
       return;
     }
-    if (!cat_name.trim()) {
+    if (!cat_pz.trim()) {
       wx.showToast({
-        title: "请输入姓名",
+        title: "请输入品种",
         icon: "none",
       });
       return;
@@ -185,54 +247,108 @@ Page({
       });
       return;
     }
-    // if (!level) {
-    //   wx.showToast({
-    //     title: "请选择级别",
-    //     icon: "none",
-    //   });
-    //   return;
-    // }
-    if (!desc) {
+    if (!color.trim()) {
       wx.showToast({
-        title: "请输入简介信息 ",
+        title: "请输入颜色",
         icon: "none",
       });
       return;
     }
-    wx.showLoading({
-      title: '报名中..',
-      mask: true
-    })
-    Api.joinMatch({
-      cat_name,
-      img,
-      sex,
-      desc,
-      level,
-      match_id
-    }).then(res => {
-      wx.hideLoading()
+    if (!eye_color.trim()) {
       wx.showToast({
-        title: '参赛成功，1.5秒自动返回',
+        title: "请输入耳朵颜色",
         icon: "none",
-        mask: true,
-      })
-      setTimeout(() => {
-        wx.navigateBack({
-          delta: 1,
-        })
-      }, 1500);
-    })
-
+      });
+      return;
+    }
+    if (!register_no.trim()) {
+      wx.showToast({
+        title: "请输入证书编号",
+        icon: "none",
+      });
+      return;
+    }
+    if (!father_name.trim()) {
+      wx.showToast({
+        title: "请输入父亲名字",
+        icon: "none",
+      });
+      return;
+    }
+    if (!father_pz.trim()) {
+      wx.showToast({
+        title: "请输入父亲品种",
+        icon: "none",
+      });
+      return;
+    }
+    if (!father_color.trim()) {
+      wx.showToast({
+        title: "请输入父亲颜色",
+        icon: "none",
+      });
+      return;
+    }
+    if (!father_register_no.trim()) {
+      wx.showToast({
+        title: "请输入父亲证书编号",
+        icon: "none",
+      });
+      return;
+    }
+    if (!mother_pz.trim()) {
+      wx.showToast({
+        title: "请输入母亲证书编号",
+        icon: "none",
+      });
+      return;
+    }
+    if (!mother_color.trim()) {
+      wx.showToast({
+        title: "请输入母亲颜色",
+        icon: "none",
+      });
+      return;
+    }
+    if (!mother_name.trim()) {
+      wx.showToast({
+        title: "请输入母亲名字",
+        icon: "none",
+      });
+      return;
+    }
+    if (!mother_register_no.trim()) {
+      wx.showToast({
+        title: "请输入母亲编号",
+        icon: "none",
+      });
+      return;
+    }
+    if (!group_id.trim()) {
+      wx.showToast({
+        title: "请选择参赛组别",
+        icon: "none",
+      });
+      return;
+    }
+    if (!match_id.trim()) {
+      wx.showToast({
+        title: "请选择FUN SHOW",
+        icon: "none",
+      });
+      return;
+    }
+    return true
 
   },
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     this.setData({
-      match_id: options.match_id
+      match_id: options?.match_id
     })
   },
 
